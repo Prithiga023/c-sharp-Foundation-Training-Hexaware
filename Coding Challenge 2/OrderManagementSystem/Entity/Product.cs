3d839abbbe1ef31e57935ad57a071bdea660753e
Prithiga023 <129827381+Prithiga023@users.noreply.github.com>
2025-04-17 13:45:06 +0530

    Create Product.cs

---
+++ Coding Challenge 2/OrderManagementSystem/Entity/Product.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public string Type { get; set; } // Electronics or Clothing
+    }
+}
