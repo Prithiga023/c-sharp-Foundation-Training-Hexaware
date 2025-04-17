d258c54751baa05036b783decabb4f8783cbb718
Prithiga023 <129827381+Prithiga023@users.noreply.github.com>
2025-04-18 00:06:18 +0530

    Create TestGetOrdersByUserDaoTests.cs

---
+++ Coding Challenge 2/OrderManagementSystem/OrderManagementSystem.Tests/TestGetOrdersByUserDaoTests.cs
@@ -0,0 +1,29 @@
+
+using NUnit.Framework;
+using Order_Management_System.dao;
+using OrderManagementSystem.Entity;
+using System.Collections.Generic; // Ensure this is included for List<T>  
+
+namespace OrderManagementSystem.Tests
+{
+    public class TestGetOrdersByUserDaoTests // Renamed class to avoid conflict  
+    {
+        private IGetOrdersByUserDao _getOrdersByUserDao;
+
+        [SetUp]
+        public void Setup()
+        {
+            _getOrdersByUserDao = new GetOrdersByUserDao(); // Ensure GetOrdersByUserDao implements IGetOrdersByUserDao  
+        }
+
+        [Test]
+        public void GetOrderByUser_ShouldReturnListOfOrders_ForValidUser()
+        {
+            var user = new User { UserId = 1001, Username = "testuser1", Role = "Customer" };
+            var orders = _getOrdersByUserDao.GetOrderByUser(user);
+
+            Assert.That(orders, Is.Not.Null); // Replace Assert.IsNotNull with Assert.That and Is.Not.Null  
+            Assert.That(orders, Is.InstanceOf<List<Order>>());
+        }
+    }
+}
