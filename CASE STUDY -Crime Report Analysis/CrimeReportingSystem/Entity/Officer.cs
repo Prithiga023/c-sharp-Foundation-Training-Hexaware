e1a9bc774a36ae31dd15fad5f9f5e47f4257ea60
Prithiga023 <129827381+Prithiga023@users.noreply.github.com>
2025-04-20 17:10:56 +0530

    Create Officer.cs

---
+++ CASE STUDY -Crime Report Analysis/CrimeReportingSystem/Entity/Officer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeReportingSystem.Entity
+{
+    public class Officer
+    {
+        public int OfficerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BadgeNumber { get; set; }
+        public string Rank { get; set; }
+        public string ContactInformation { get; set; }
+        public int AgencyID { get; set; }
+
+        public Officer() { }
+
+        public Officer(int officerID, string firstName, string lastName, string badgeNumber, string rank, string contactInfo, int agencyID)
+        {
+            OfficerID = officerID;
+            FirstName = firstName;
+            LastName = lastName;
+            BadgeNumber = badgeNumber;
+            Rank = rank;
+            ContactInformation = contactInfo;
+            AgencyID = agencyID;
+        }
+    }
+}
