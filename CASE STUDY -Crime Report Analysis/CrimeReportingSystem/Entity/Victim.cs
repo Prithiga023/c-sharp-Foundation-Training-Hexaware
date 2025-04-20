655476b197e39e6b6479dd603fdea070dbef079a
Prithiga023 <129827381+Prithiga023@users.noreply.github.com>
2025-04-20 17:13:11 +0530

    Create Victim.cs

---
+++ CASE STUDY -Crime Report Analysis/CrimeReportingSystem/Entity/Victim.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeReportingSystem.Entity
+{
+    public class Victim
+    {
+        public int VictimID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string ContactInformation { get; set; }
+
+        public Victim() { }
+
+        public Victim(int victimID, string firstName, string lastName, DateTime dateOfBirth, string gender, string contactInfo)
+        {
+            VictimID = victimID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            ContactInformation = contactInfo;
+        }
+    }
+}
